cdcdf85e9958ed1f7ec4fe49a6257ee5cca29e28
Frederik Bruhn Mikkelsen <frederik310@gmail.com>
2014-09-14 16:31:26 +0200

    updated robots.txt parser and added functionality to check whether an url is legal to visit

---
+++ RobotsParser/RobotsTxt.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WebCrawler
+{
+    public class RobotsTxt
+    {
+        private const string HTTP = "http://";
+        private const string HTTPS = "https://";
+
+        private Dictionary<string, Politeness> politenesses = new Dictionary<string, Politeness>();
+        public Uri Domain { get; set; }
+        public List<string> Sitemaps { get; set; }
+
+        public RobotsTxt(string domain)
+        {
+            if (!domain.StartsWith(HTTP) && !domain.StartsWith(HTTPS))
+                domain = string.Format("{0}{1}", HTTP, domain);
+
+            Domain = new Uri(domain);
+        }
+
+        public void PutPoliteness(string agent, Politeness politeness)
+        {
+            politenesses.Add(agent, politeness);
+        }
+
+        public Politeness GetPoliteness(string agent)
+        {
+            return politenesses[agent];
+        }
+
+        public bool HasAgent(string agent)
+        {
+            return politenesses.ContainsKey(agent);
+        }
+
+        public bool CanVisit(string agent, Uri uri)
+        {
+            bool result = true;
+
+            if (!politenesses.ContainsKey(agent))
+                return result;
+
+            string url = uri.AbsoluteUri;
+
+            try
+            {
+                foreach (var politeness in politenesses[agent].Disallows)
+                {
+                    string pattern = string.Format(".*{0}.*", politeness.Replace("*", ".*").Replace("$", ".*"));
+                    Regex regEx = new Regex(pattern);
+                    if (regEx.Match(url).Success)
+                    {
+                        result = false;
+                        break;
+                    }
+                }
+
+                foreach (var politeness in politenesses[agent].Allows)
+                {
+                    string pattern = Regex.Escape(politeness);
+                    pattern = string.Format(".*{0}.*", politeness.Replace("\\*", ".*"));
+                    Regex regEx = new Regex(pattern);
+                    if (regEx.Match(url).Success)
+                    {
+                        break;
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                result = false;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ RobotsParser/RobotsTxtParser.cs
@@ -0,0 +1,104 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Security.Policy;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace WebCrawler
+{
+    public class RobotsTxtParser
+    {
+        private const string ROBOT_TXT = "robots.txt";
+
+        private static Regex agentRegex = new Regex(@"User-agent:\s*(.*)", RegexOptions.IgnoreCase | RegexOptions.IgnorePatternWhitespace);
+        private static Regex disallowRegex = new Regex(@"Disallow:\s*(.*)", RegexOptions.IgnoreCase | RegexOptions.IgnorePatternWhitespace);
+        private static Regex allowRegex = new Regex(@"Allow:\s*(.*)", RegexOptions.IgnoreCase | RegexOptions.IgnorePatternWhitespace);
+        private static Regex sitemapRegex = new Regex(@"Sitemap:\s*(.*)", RegexOptions.IgnoreCase | RegexOptions.IgnorePatternWhitespace);
+        
+        public RobotsTxt Parse(string domainUrl)
+        {
+
+            RobotsTxt robotTxt = new RobotsTxt(domainUrl);
+            Uri uri = new Uri(robotTxt.Domain, ROBOT_TXT);
+            Politeness currentPoliteness = null;
+
+            var reader = GetStream(uri);
+            if (reader != null)
+            {
+                string line;
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    var agentMatch = agentRegex.Match(line);
+
+                    if (agentMatch.Success)
+                    {
+                        string agent = agentMatch.Groups[1].Value;
+
+                        if (!robotTxt.HasAgent(agent))
+                        {
+                            currentPoliteness = new Politeness();
+                            robotTxt.PutPoliteness(agent, currentPoliteness);
+                        }
+                        else
+                            currentPoliteness = robotTxt.GetPoliteness(agent);
+                    }
+                    else
+                    {
+                        var disallowMatch = disallowRegex.Match(line);
+                        var allowMatch = allowRegex.Match(line);
+
+                        if (disallowMatch.Success)
+                        {
+                            if (currentPoliteness != null)
+                                currentPoliteness.Disallows.Add(disallowMatch.Groups[1].Value);
+                        }
+                        else if (allowMatch.Success)
+                        {
+                            if (currentPoliteness != null)
+                                currentPoliteness.Allows.Add(allowMatch.Groups[1].Value);
+                        }
+                    }
+
+
+                    var sitemapMatch = sitemapRegex.Match(line);
+
+                    if (!sitemapMatch.Success) continue;
+                    if (currentPoliteness != null)
+                        robotTxt.Sitemaps.Add(sitemapMatch.Groups[1].Value);
+                }
+            }
+
+            return robotTxt;
+        }
+
+        private StreamReader GetStream(Uri uri)
+        {
+            StreamReader reader = null;
+            try
+            {
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+
+                // Sends the HttpWebRequest and waits for the response.
+                HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+                // Gets the stream associated with the response.
+                Stream stream = response.GetResponseStream();
+
+                // Pipes the stream to a higher level stream reader with the required encoding format. 
+                Encoding encoding = Encoding.GetEncoding("utf-8");
+                reader = new StreamReader(stream, encoding);
+            }
+            catch (WebException e)
+            {
+            }
+            return reader;
+        }
+
+    }
+}
