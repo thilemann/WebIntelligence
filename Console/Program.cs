9233592ad6e21d7ace787f9da27418c18bac5b96
Stefan Thilemann <thilemann@gmail.com>
2014-09-17 00:24:29 +0200

    Cleaned up file

--- Console/Program.cs
+++ Console/Program.cs
@@ -12,17 +12,11 @@ namespace WebCrawler
         static void Main(string[] args)
         {
             Crawler crawler = new Crawler("Ressources\\Seeds.txt");
+
             Console.WriteLine("Starting...");
             crawler.Start(1000);
+            
             Console.WriteLine("Finished crawling...");
-
-            //Uri uri = new Uri("http://www.kabelchristensen.dk/Sitemap");
-            //Sitemap smap = new Sitemap(uri);
-
-            //foreach (var url in smap.Contents)
-            //{
-            //    Console.WriteLine(url.Location + "\t\t\t" + url.LastModified);
-            //}
             Console.WriteLine("Press any key to exit...");
             Console.ReadLine();
         }
