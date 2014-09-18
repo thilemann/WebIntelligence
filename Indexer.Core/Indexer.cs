45791ca39f64e517ed3255ba44092a871336e732
Stefan Thilemann <thilemann@gmail.com>
2014-09-18 14:05:44 +0200

    Initial commit of the indexer

---
+++ Indexer.Core/Indexer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Indexer.Token;
+using Indexer.Linguistics;
+
+namespace Indexer.Core
+{
+    class Indexer
+    {
+        private Tokenizer tokenizer;
+
+        private Dictionary<int, string> postingMap;
+        private SortedDictionary<string, Term> terms;
+
+        public Indexer()
+        {
+            tokenizer = new Tokenizer();
+            postingMap = new Dictionary<int, string>();
+            terms = new SortedDictionary<string, Term>();
+        }
+
+        public void Start(string uri, string fileContent)
+        {
+            int postingId = uri.GetHashCode();
+            postingMap.Add(postingId, uri);
+            List<string> unStemmedTokens = tokenizer.Tokenize(fileContent);
+            foreach (var token in unStemmedTokens)
+            {
+                Stemmer stemmer  = new Stemmer();
+                stemmer.Stem(token);
+                string stemmedToken = stemmer.ToString();
+                
+                Term term = new Term();
+                term.AddPosting(postingId);
+                terms.Add(stemmedToken, term);
+            }
+            unStemmedTokens = null;
+        }
+    }
+}
