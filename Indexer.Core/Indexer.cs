bb9ae6d0d2dc774b849460172f2c8f2275b7b3da
Stefan Thilemann <thilemann@gmail.com>
2014-10-01 13:47:39 +0200

    Added method for returning an Uri from a hashcode

--- Indexer.Core/Indexer.cs
+++ Indexer.Core/Indexer.cs
@@ -16,6 +16,9 @@ namespace Indexing.Core
         private readonly Dictionary<int, string> _postingMap;
         private readonly SortedDictionary<string, Term> _terms;
 
+        public int TotalDocuments { get { return _postingMap.Count(); } }
+        public SortedDictionary<string, Term> Terms { get { return _terms; } }
+
         public Indexer()
         {
             _tokenizer = new Tokenizer();
@@ -38,20 +41,23 @@ namespace Indexing.Core
                 Term term;
                 if (_terms.ContainsKey(stemmedToken))
                 {
-                    if (_terms[stemmedToken].ContainsPosting(postingId))
-                    {
-                        continue;
-                    }
                     term = _terms[stemmedToken];
-                    term.AddPosting(postingId);
                 }
                 else
                 {
                     term = new Term(stemmedToken);
-                    term.AddPosting(postingId);
                     _terms.Add(stemmedToken, term);
                 }
+                term.AddPosting(postingId);
             }
+        }
+
+        public string GetUrlFromHash(int hashCode)
+        {
+            if (_postingMap.ContainsKey(hashCode))
+                return _postingMap[hashCode];
+            else
+                return null;
         }
     }
 }
