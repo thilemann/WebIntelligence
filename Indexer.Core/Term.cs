597e05938ce43b303c88bb8169d7c681f9e0f6e6
Stefan Thilemann <thilemann@gmail.com>
2014-10-01 13:48:18 +0200

    Changed list used to store postings

--- Indexer.Core/Term.cs
+++ Indexer.Core/Term.cs
@@ -1,14 +1,16 @@
 ﻿using System;
-using System.Collections;
+using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Indexing.Core
 {
-    class Term
+    public class Term
     {
-        private readonly SortedList _postings;
+        private readonly SortedList<int, int> _postings;
+
+        public SortedList<int, int> Postings { get { return _postings; } }
 
         public int Frequency {
             get
@@ -19,7 +21,7 @@ namespace Indexing.Core
 
         public Term(string term)
         {
-            _postings = new SortedList();
+            _postings = new SortedList<int, int>();
         }
 
         public bool ContainsPosting(int id)
@@ -29,7 +31,22 @@ namespace Indexing.Core
 
         public void AddPosting(int id)
         {
-            _postings.Add(id, id);
+            if (_postings.ContainsKey(id))
+            {
+                _postings[id] = _postings[id] + 1;
+            }
+            else
+	        {
+                _postings.Add(id, 1);
+	        }
+        }
+
+        public int GetTermFrequency(int id)
+        {
+            if (_postings.ContainsKey(id))
+                return _postings[id];
+            else
+                return 0;
         }
     }
 }
